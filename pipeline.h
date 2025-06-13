5f3dd0e7b2d271bc07100c9f5eb8d07e9aad69b3
rodchs <128987477+rodchs@users.noreply.github.com>
2025-06-13 17:13:58 -0300

    Update pipeline.h

--- pipeline.h
+++ pipeline.h
@@ -30,8 +30,28 @@ typedef struct {
 
 typedef struct{
     int pc;
-    Instrucao inst;
+    char inst[17];
 } pipeline_estagio_1;
+
+typedef struct{
+    int pc;
+    int A;
+    int B;
+    int f_jump;
+    Instrucao inst;
+} pipeline_estagio_2;
+
+typedef struct{
+    int pc;
+    int address;
+    int ULA_out;
+    int f_zero;
+    int f_jump;
+    int f_branch;
+    int rd;
+    int branch_address;
+    int dado_escrita;
+} pipeline_estagio_3;
 
 void controle(int *estagio_inst, pipeline_estagio_1 *estagio_1, pipeline_estagio_2 *estagio_2, int *pc, Mem_p *mem_p, BancoRegistradores *BR);int ula(int a, int b, int op);
 void carregarMemoria(Mem_p *mem_p);
